b03d56ca5ad875f245ef77f8a55ab5832016ebfe
augustinedevops <164868777+Augustinedevops@users.noreply.github.com>
2024-04-02 15:22:16 +0800

    Add files via upload

--- Creditcardchecker2.0.py
+++ Creditcardchecker2.0.py
@@ -1,12 +1,12 @@
-card = [4546568735994633, 5566892315260104, 373521757791003, 222]
-
-for i in card:
-    str_i = str(i)
-    if str_i[0] == '4' and (len(str_i) == 13 or len(str_i) == 16):
-        print(i, "is a VISA card")
-    elif 51 <= int(str_i[:2]) <= 55 and len(str_i) == 16:
-        print(i, "is a Mastercard")
-    elif str_i[:2] in ('34', '37') and len(str_i) == 15:
-        print(i, 'is an AMEX card')
+for i in range(len(list)):
+    number = list[i]
+    if i == 0 and number > list[i + 1]:
+        print(number * "*", "Peak")
+    elif 0 < i < len(list) - 1 and list[i - 1] < number > list[i + 1]:
+        print(number * "*", "Peak")
+    elif 0 < i < len(list) - 1 and list[i - 1] <= number >= list[i + 2]:
+        print(number * "*", "Peak")
+    elif i == len(list) - 1 and number > list[i - 1]:
+        print(number * "*", "Peak")
     else:
-        print(i, "is an invalid card")
+        print(number * "*")
